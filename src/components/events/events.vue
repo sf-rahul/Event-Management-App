76db90044076d6b4b6e5daee553e71ab902dffb7
rahulrsinha <rahul.sinha.raw@gmail.com>
2020-06-06 21:08:30 +0530

    Added page to view events.

--- src/components/events/events.vue
+++ src/components/events/events.vue
@@ -14,7 +14,44 @@
           outlined
           tile
         >
-          One of three columns
+          <template>
+  <v-card
+    class="mx-auto"
+    max-width="400"
+  >
+    <v-img
+      class="white--text align-end"
+      height="200px"
+      src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+    >
+      <v-card-title>Top 10 Australian beaches</v-card-title>
+    </v-img>
+
+    <v-card-subtitle class="pb-0">Number 10</v-card-subtitle>
+
+    <v-card-text class="text--primary">
+      <div>Whitehaven Beach</div>
+
+      <div>Whitsunday Island, Whitsunday Islands</div>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn
+        color="orange"
+        text
+      >
+        Share
+      </v-btn>
+
+      <v-btn
+        color="orange"
+        text
+      >
+        Register
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
         </v-card>
       </v-col>
     </v-row>
@@ -78,7 +115,7 @@ export default {
 <style scoped>
 
 .container{
-  margin-top: 30px;
+  margin-top: 20px;
 }
 
 </style>
